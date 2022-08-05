807f9055628bc09aa95cdb13ea3e35fbef45bb2d
Sudipto Dutta <sudiptodutta_1992@outlook.com>
2022-08-05 19:11:19 +0530

    MIgrations added

--- Program.cs
+++ Program.cs
@@ -1,8 +1,12 @@
 using Commander.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<CommanderContext>(opt => opt.UseSqlServer
+    (builder.Configuration.GetConnectionString("CommanderConnection")));
 
 builder.Services.AddControllers();
 
